8169874c4e51a3abec4d314a665ecb4b689a1b17
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-08 14:01:12 +0530

    Calculating wages till total working hour or day is reached fora month

---
+++ UC6EmpWageMonthHr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+Fulltime=2;
+Parttime=1;
+EmpRatePerHour=20;
+MaxWorkingDays=20;
+MaxHours=100;
+TotWorkingDays=0;
+TotWorkingHours=0;
+
+while [[ $TotWorkingHours -lt $MaxHours && $TotWorkingDays -lt $MaxHours ]]
+do
+ ((TotWorkingDays++))
+ Check=$((RANDOM % 3))
+
+case $Check in
+   $Parttime)
+      EmpHour=8 ;;
+   $Fulltime)
+      EmpHour=12 ;;
+   *)
+      EmpHour=0 ;;
+esac
+	TotWorkingHours=$((TotWorkingHour + EmpHours));
+done
+   Salary=$(( TotWorkingHours * EmpRatePerHour)); 
+
